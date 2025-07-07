2d0c8fa93e198241ec2f5ac04f3dbbf00aaa57b0
Jose Nimo <nimojose@gmail.com>
2025-07-07 13:39:45 +0200

    volcano_refactor_testing

--- src/opendvp/plotting/volcano.py
+++ src/opendvp/plotting/volcano.py
@@ -1,16 +1,18 @@
+from collections.abc import Mapping
 from typing import Any
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 from adjustText import adjust_text
 from anndata import AnnData
+from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 
 
 def volcano(
     adata: AnnData,
-    x: str = "log2_FC",
-    y: str = "-log10(p_val_corr)_BH",
+    x: str = "mean_diff",
+    y: str = "-log10_p_corr",
     significant: bool = False,
     FDR: float | None = None,
     significant_metric : str = "p_corr",
@@ -18,10 +20,10 @@ def volcano(
     group1: str | None = None,
     group2: str | None = None,
     return_fig: bool = False,
+    ax : Axes | None = None,
     highlight_genes: list[str] | None = None,
     show_highlighted_genes_names: bool = True,
-    ax: Any | None = None,
-    **kwargs
+    **kwargs : Mapping[str, Any]
 ) -> Figure | None:
     """Plot a volcano plot from an AnnData object.
 
@@ -59,23 +61,27 @@ def volcano(
     fig : matplotlib.figure.Figure or None
         The `fig` object if `return_fig=True`, otherwise None.
     """
-    adata_copy = adata.copy()
-    df = adata_copy.var
-    if ax is None:
-        fig, ax = plt.subplots(figsize=(10, 10))
+    # plotting logic
+    internal_ax = ax is None
+    if internal_ax:
+        fig, ax = plt.subplots()
     else:
         fig = ax.figure
+    # plotting logic
+
+    #objects
+    adata_copy = adata.copy()
+    df = adata_copy.var
+
     sns.scatterplot(x=df[x], y=df[y], ax=ax, color='gray', s=20, edgecolor=None, **kwargs)
-    if group1 is not None and group2 is not None:
-        ax.set_xlabel(f"Difference in mean protein expression (log2)\n{group1} (right) vs {group2} (left)")
-    else:
-        ax.set_xlabel(x)
-    ax.set_ylabel("-log10 corrected p-value BH")
+
+    
     if significant:
         if FDR is None:
             raise ValueError("FDR must be specified if significant=True.")
         sig_df = df[df[significant_metric] < FDR]
         ax.scatter(sig_df[x], sig_df[y], color="red", s=20)
+
     if tag_top:
         tag_df = df.sort_values(by=y, ascending=False).head(tag_top)
         df_left = tag_df[tag_df[x] < 0]
@@ -94,11 +100,14 @@ def volcano(
             expand_points=(1.2, 1.2),
             arrowprops=dict(arrowstyle="-", color='black', lw=0.5, alpha=0.5)
         )
+    
     if highlight_genes is not None:
         highlight_genes_found = [g for g in highlight_genes if g in df.index]
-        if highlight_genes_found:
+
+        if len(highlight_genes_found) > 0:
             highlight_df = df.loc[highlight_genes_found]
             ax.scatter(highlight_df[x], highlight_df[y], color="blue", s=20, edgecolor='black', zorder=5, alpha=0.7)
+            
             if show_highlighted_genes_names:
                 texts_highlight = [
                     ax.text(row[x], row[y], idx, ha='center', va='bottom', fontsize=9, fontweight='bold', color='blue')
@@ -110,9 +119,19 @@ def volcano(
                     expand_points=(1.2, 1.2),
                     arrowprops=dict(arrowstyle="-", color='blue', lw=0.5, alpha=0.5)
                 )
+    
+    if group1 is not None and group2 is not None:
+        ax.set_xlabel(f"Difference in mean protein expression (log2)\n{group1} (right) vs {group2} (left)")
+    else:
+        ax.set_xlabel(x)
+    ax.set_ylabel(y)
+    
     ax.grid(False)
+
+    # plotting logic
     if return_fig:
         return fig
-    else:
+    elif internal_ax:
         plt.show()
-        return None
+    return None
+    # plotting logic
