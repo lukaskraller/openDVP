d5781c6c947da7f206244b78c1abdeee00f7d279
Jose Nimo <nimojose@gmail.com>
2025-07-13 21:55:13 +0200

    boxplots

--- src/opendvp/plotting/__init__.py
+++ src/opendvp/plotting/__init__.py
@@ -3,6 +3,7 @@ from .coefficient_of_variation import coefficient_of_variation
 from .correlation_heatmap import correlation_heatmap
 from .density import density
 from .dual_axis_boxplots import dual_axis_boxplots
+from .feature_comparison_boxplot import feature_comparison_boxplot
 from .imputation_qc import imputation_qc
 from .pca_loadings import pca_loadings
 from .plot_graph_network import plot_graph_network
@@ -12,16 +13,17 @@ from .upset import upset
 from .volcano import volcano
 
 __all__ = [
-    "rankplot",
     "abundance_histograms",
+    "coefficient_of_variation",
     "correlation_heatmap",
     "density",
-    "stacked_barplot",
-    "volcano",
-    "plot_graph_network",
-    "coefficient_of_variation",
     "dual_axis_boxplots",
+    "feature_comparison_boxplot",
     "imputation_qc",
     "pca_loadings",
+    "plot_graph_network",
+    "rankplot",
+    "stacked_barplot",
     "upset",
+    "volcano",
 ]
