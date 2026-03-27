461e051604259be4f21fb4d4615f33df7e1e6c5c
lukaskraller <lukas.kraller@mdc-berlin.de>
2026-03-27 16:03:54 +0100

    added test

--- tests/plotting/test_correlation_heatmap.py
+++ tests/plotting/test_correlation_heatmap.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import pytest
 from anndata import AnnData
+#test
 
 matplotlib.use("Agg")  # Use non-interactive backend for tests
 from opendvp.plotting import correlation_heatmap
